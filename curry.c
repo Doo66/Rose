87f0fc3b7853bd3693a2a0bd969dd15698f5f771
Pzd_Rose <doo_magic@126.com>
2016-04-06 20:21:41 -0700

    my_swap

--- curry.c
+++ curry.c
@@ -7,18 +7,25 @@
 
 #include<stdio.h>
 
-void Swap(int* p1, int* p2)
+/*void Swap(int* p1, int* p2)
 {
 	int tmp = *p1;
 	*p1 = *p2;
 	*p2 = tmp;
+}*/
+
+void Swap(int&p1,int&p2)
+{
+	int tmp = p1;
+	p1 = p2;
+	p2 = tmp;
 }
 int main()
 {
 	int a,b;
 	printf("Please enter two numbers: ");
 	scanf("%d %d",&a, &b);
-	Swap(&a, &b);
+	Swap(a, b);
 	printf("a = %d, b = %d\n", a ,b);
 	return 0;
 }
