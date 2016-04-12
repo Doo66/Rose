200378c3a545e730166d9561229960af20f970ff
Pzd流川枫 <doo_magic@126.com>
2016-04-12 10:59:21 +0800

    Update curry.c

--- curry.c
+++ curry.c
@@ -7,19 +7,19 @@
 
 #include<stdio.h>
 
-/*void Swap(int* p1, int* p2)
+void Swap(int* p1, int* p2)
 {
 	int tmp = *p1;
 	*p1 = *p2;
 	*p2 = tmp;
-}*/
+}
 
-void Swap(int&p1,int&p2)
+/*void Swap(int&p1,int&p2)
 {
 	int tmp = p1;
 	p1 = p2;
 	p2 = tmp;
-}
+}*/
 int main()
 {
 	int a,b;
